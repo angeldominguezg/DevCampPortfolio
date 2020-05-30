935a0b1f34b13294e8d2549a7d44b1693c5a2551
Angel Dominguez <angeldominguezg@gmail.com>
2020-05-30 12:11:05 -0400

    Adding change status featured blog section

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
 
   get 'pages/contact'
 
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
