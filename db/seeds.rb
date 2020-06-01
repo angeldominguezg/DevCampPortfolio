96d5d263f53c86a150f0d4e40983409d674d57d5
Angel Dominguez <angeldominguezg@gmail.com>
2020-06-01 07:10:43 -0400

    implemented custom scopes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
+puts "3 Topics created!"
+
 # Blog Seed
 10.times do |blog|
   Blog.create!(
     title:"My blog Post #{blog}", 
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eros leo, ut pretium sapien ornare non. Sed ut tincidunt ante, vitae tempor erat. Donec efficitur purus lorem, at consequat diam iaculis nec. Morbi non est vel eros faucibus ultrices. Maecenas quis metus sit amet leo consequat finibus. Pellentesque vel justo eu magna rhoncus sagittis in id lorem. Etiam iaculis augue nunc, sed tincidunt lacus euismod vitae. Quisque ut lacinia odio. Suspendisse iaculis ac nisl ut aliquet. Quisque interdum nulla at dictum interdum. Maecenas lacinia in dolor semper iaculis. Nullam eu enim lorem. Integer blandit sem in orci rhoncus, a congue turpis facilisis. In lobortis aliquet mi vitae imperdiet."
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eros leo, ut pretium sapien ornare non. Sed ut tincidunt ante, vitae tempor erat. Donec efficitur purus lorem, at consequat diam iaculis nec. Morbi non est vel eros faucibus ultrices. Maecenas quis metus sit amet leo consequat finibus. Pellentesque vel justo eu magna rhoncus sagittis in id lorem. Etiam iaculis augue nunc, sed tincidunt lacus euismod vitae. Quisque ut lacinia odio. Suspendisse iaculis ac nisl ut aliquet. Quisque interdum nulla at dictum interdum. Maecenas lacinia in dolor semper iaculis. Nullam eu enim lorem. Integer blandit sem in orci rhoncus, a congue turpis facilisis. In lobortis aliquet mi vitae imperdiet.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -27,13 +37,26 @@ end
 puts "5 skills created!"
 
 # Portfolio seed
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
-    title: "Portfolio Title #{portfolio_item}",
-    subtitle: "My great service",
+    title: "Portfolio Item #{portfolio_item}",
+    subtitle: "Ruby on rails",
     body: "Nunc aliquet enim justo, ac luctus eros tempor accumsan. Maecenas consequat vehicula condimentum. Nulla mattis eros et malesuada tincidunt. Aliquam at mauris nec est porttitor pellentesque. Maecenas hendrerit vitae tellus sed cursus. Mauris in bibendum urna. Donec in tempor nisi, semper semper odio. Sed id orci magna.",
     main_image: "https://via.placeholder.com/600x200",
     thumb_image: "https://via.placeholder.com/350x200",
   )
 end
-  puts "9 portfolio created!"
+
+puts "8 portfolio Ruby created!"
+
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio Item #{portfolio_item}",
+    subtitle: "Angular",
+    body: "Nunc aliquet enim justo, ac luctus eros tempor accumsan. Maecenas consequat vehicula condimentum. Nulla mattis eros et malesuada tincidunt. Aliquam at mauris nec est porttitor pellentesque. Maecenas hendrerit vitae tellus sed cursus. Mauris in bibendum urna. Donec in tempor nisi, semper semper odio. Sed id orci magna.",
+    main_image: "https://via.placeholder.com/600x200",
+    thumb_image: "https://via.placeholder.com/350x200",
+  )
+end
+puts "1 portfolio Angular created!"
