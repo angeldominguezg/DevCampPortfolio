15632329a5eef86617b6777591271113701ab4f5
Angel Dominguez <angeldominguezg@gmail.com>
2020-06-04 06:35:58 -0400

    Implemented basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'pages#home'
 
   resources :portfolios, except: [:show]
 
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-
-  root to: 'pages#home'
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
