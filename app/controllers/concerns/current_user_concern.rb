81777d5784b4f1e9de701472ee77a8ff7e5c6f5d
Angel Dominguez <angeldominguezg@gmail.com>
2020-06-11 06:55:50 -0400

    Added navbar and added defauld head content

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -3,4 +3,4 @@ module CurrentUserConcern
     def current_user
         super || OpenStruct.new(name: "Guest User", first_name: "Guest", las_tname: "User", email: "g_user@mail.com")
     end
-  end
+end
