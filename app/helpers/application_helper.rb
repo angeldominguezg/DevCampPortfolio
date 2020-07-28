2d63d600041ca2d37a586c7bc14106bfb9ae3434
Angel Dominguez <angeldominguezg@gmail.com>
2020-07-28 12:57:39 -0400

    Navigatio helper with active link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,14 +23,44 @@ module ApplicationHelper
     end
   end
 
+
+  def nav_items
+  [
+    {
+      url: root_path,
+      title: 'Home'
+    },
+    {
+      url: about_path,
+      title: 'About Me'
+    },
+    {
+      url: contact_path,
+      title: 'Contact'
+    },
+    {
+      url: blogs_path,
+      title: 'Blog'
+    },
+    {
+      url: portfolios_path,
+      title: 'Portfolios'
+    },
+  ]
+  end
+
   def nav_helper style, tag_type
-nav_links = <<NAV
-<#{tag_type}><a href="#{root_path}" class="#{style}">Home</#{tag_type}>
-<#{tag_type}><a href="#{about_path}" class="#{style}">About Me</#{tag_type}>
-<#{tag_type}><a href="#{contact_path}" class="#{style}">Contact</#{tag_type}>
-<#{tag_type}><a href="#{blogs_path}" class="#{style}">Blog</#{tag_type}>
-<#{tag_type}><a href="#{portfolios_path}" class="#{style}">Portfolios</#{tag_type}>
-NAV
+
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</#{tag_type}>"
+    end
+
     return nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
   end
 end
